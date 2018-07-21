6c9dcf49dd46ad28445fad2cba7c5fc6bfaad58c
Alex Harrison <alexjharrison@gmail.com>
2018-07-21 15:48:11 -0400

    made route for public images

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,8 +9,15 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname,"../public/survery.html"));
     });
 
+    app.get("/images/:imageName",(req,res)=>{
+        var name = req.params.imageName;
+        res.sendFile(path.join(__dirname,"../public/images/"+name));
+    })
+
     app.get("*",(req,res)=>{
         res.status(404);
         res.sendFile(path.join(__dirname,"../public/404.html"));
     });
+
+    
 };
